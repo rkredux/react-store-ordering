3f1ad687bcb3bba08593a775076800a1086f63eb
Rahul <rkredux@gmail.com>
2017-05-11 10:06:56 -0400

    Add updates to fish component

--- fish.js
+++ fish.js
@@ -1,8 +1,21 @@
 import React from "react";
+import {formatPrice} from "../helpers.js"; 
 
 class Fish extends React.Component {
+
+
 	render(){
-		return <li className="menu-fish">Fish</li>
+		const {details, index} = this.props; //ES6 object destructuring
+		const isAvailable = details.status === "available"; 
+		const buttonText = isAvailable ? "Add To Order" : "Sold Out"; 
+		return <li className="menu-fish">
+		<img src={details.image} alt={details.name}/>
+		<h3 className="fish-name">{details.name}
+		<span className="price">{formatPrice(details.price)}</span>
+		</h3>
+		<p>{details.desc}</p>
+		<button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
+		</li>
 	}
 }; 
 
